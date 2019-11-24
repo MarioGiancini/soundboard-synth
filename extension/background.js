8609e7d09cbe85e25d37792b7827b13bfd9a193f
Mario Giancini <mario.giancini@gmail.com>
2019-11-24 00:43:05 -0500

    Only set storage for a page if there are found sounds

--- extension/background.js
+++ extension/background.js
@@ -22,58 +22,60 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (typeof request.type !== 'undefined' && request.type === 'loaded') {
         console.log('REQUEST: loaded', request);
 
-        if (request.total > 0) {
-            chrome.browserAction.setBadgeText({"text": request.total});
-            chrome.browserAction.setBadgeBackgroundColor({"color": "#00d1b2"});
-        } else {
-            chrome.browserAction.setBadgeText({"text": ""});
-        }
-
         let soundboardUrls = [],
             keyMap = {};
 
-        // Get currently stored sound files and key mappings by unique url
-        // Do storage of update within callback of get request
-        chrome.storage.local.get([request.url], function(result) {
-            if (result[request.url] && typeof result[request.url] !== 'undefined' && typeof result[request.url].urls !== 'undefined') {
-                console.log('Soundboard files currently are ', request.url, result);
-                soundboardUrls = result[request.url].urls;
-            } else {
-                console.log('No soundboard found at:', request.url, result);
-            }
+        if (request.total > 0) {
+            chrome.browserAction.setBadgeText({"text": request.total});
+            chrome.browserAction.setBadgeBackgroundColor({"color": "#00d1b2"});
 
-            if (result[request.url] && typeof result[request.url].keyMap !== 'undefined') {
-                console.log('Keymaps currently are ', result[request.url].keyMap);
-                keyMap = result[request.url].keyMap;
-            } else {
-                console.log('No keymaps found at:', request.url, result);
-            }
+            // Get currently stored sound files and key mappings by unique url
+            // Do storage of update within callback of get request
+            chrome.storage.local.get([request.url], function(result) {
+                if (result[request.url] && typeof result[request.url] !== 'undefined' && typeof result[request.url].urls !== 'undefined') {
+                    console.log('Soundboard files currently are ', request.url, result);
+                    soundboardUrls = result[request.url].urls;
+                } else {
+                    console.log('No soundboard found at:', request.url, result);
+                }
 
-            // Combine any stored urls with new found urls and only store unique
-            const allUrls = request.urls.concat(soundboardUrls).filter(function (resource, index, self) {
-                return typeof resource !== "undefined" && self.indexOf(resource) === index;
+                if (result[request.url] && typeof result[request.url].keyMap !== 'undefined') {
+                    console.log('Keymaps currently are ', result[request.url].keyMap);
+                    keyMap = result[request.url].keyMap;
+                } else {
+                    console.log('No keymaps found at:', request.url, result);
+                }
+
+                // Combine any stored urls with new found urls and only store unique
+                const allUrls = request.urls.concat(soundboardUrls).filter(function (resource, index, self) {
+                    return typeof resource !== "undefined" && self.indexOf(resource) === index;
+                });
+
+                // chrome.storage.local.clear(function () {
+                //     console.log('Soundboards Cleared.');
+                // });
+
+                let storeSoundboard = {
+                    urls: allUrls,
+                    keyMap: keyMap
+                };
+
+                chrome.storage.local.set({[request.url]: storeSoundboard}, function () {
+                    console.log('Soundboards updated.', {[request.url]: storeSoundboard});
+                });
+
+                // Now add other info to pass along that does need to be stored
+                storeSoundboard.url = request.url;
+                storeSoundboard.message = "Soundboards updated";
+
+                sendResponse(storeSoundboard);
+
             });
 
-            // chrome.storage.local.clear(function () {
-            //     console.log('Soundboards Cleared.');
-            // });
 
-            let storeSoundboard = {
-                urls: allUrls,
-                keyMap: keyMap
-            };
-
-            chrome.storage.local.set({[request.url]: storeSoundboard}, function () {
-                console.log('Soundboards updated.', {[request.url]: storeSoundboard});
-            });
-
-            // Now add other info to pass along that does need to be stored
-            storeSoundboard.url = request.url;
-            storeSoundboard.message = "Soundboards updated";
-
-            sendResponse(storeSoundboard);
-
-        });
+        } else {
+            chrome.browserAction.setBadgeText({"text": ""});
+        }
 
     }
     // else if (request.type === "speed") {
